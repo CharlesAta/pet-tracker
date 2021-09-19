87780e7217f1c48e6fc8cb714ea16ac55cb071d2
Lynn Yang <lynnyangnc@gmail.com>
2021-09-19 00:07:00 -0400

    add email function with nodemailer and express-handlebar

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,8 @@
 const PostModel = require("../models/post")
 const fs = require("fs")
 const axios = require('axios');
+const nodemailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
 
 module.exports = {
     createPost,
@@ -11,6 +13,23 @@ function base64_encode(image){
     let bitmap = fs.readFileSync(image);
     return bitmap.toString('base64');
 }
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+transporter.use("compile",hbs({
+    viewEngine:{
+       partialsDir:"views",
+       defaultLayout:""
+   },
+    viewPath:"views",
+    extName:".handlebars"
+}));
 
 async function createImage(req, res){
     let image = base64_encode(req.files.file.file);
@@ -37,7 +56,27 @@ async function createPost(req, res){
          let created = await PostModel.create(req.body.petState)
          console.log("created", created)
          res.status(200).json('ok')
+         let type = created.photo.match(/\.[0-9a-z]+$/i)[0]
+        if(created.status === "lost"){
+            let mailOptions = {
+                from: process.env.EMAIL_ADDRESS,
+                to: 'lynnyangnc@gmail.com',
+                subject: 'EMERGENCY - PET LOST',
+                template:'main',
+                attachments:[
+                    { filename: created.name+type, path: created.photo} 
+                ]
+              };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                    });
+        }
       } catch(err) {
          res.status(400).json(err);
-      }
+    }
 }
+
