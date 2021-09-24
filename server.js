906ab82285aeccd1cf87e9c84eff47712a7a4ddd
Lynn Yang <lynnyangnc@gmail.com>
2021-09-23 23:53:51 -0400

    Deploy to Heroku

--- server.js
+++ server.js
@@ -28,17 +28,9 @@ app.use(function (err, req, res, next) {
   res.status(500).json(err);
 });
 
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-}
 
 const port = process.env.PORT || 3001;
 
